42960837610a91050024805bc67577a59b2fd490
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-07-02 16:31:01 -0700

    added projects

--- myProjects.js
+++ myProjects.js
@@ -1,20 +1,48 @@
-import maduwedding from "./src/assets/images/tochiandyvonne.png"
-
+import maduwedding from "./src/assets/images/tochiandyvonne.png";
+import fittrack from "./src/assets/images/fittrack_mock.jpeg";
+import portfolio_site from "./src/assets/images/darryladigwu_mock.jpeg"
 export default [
   {
     id: 1,
-    title: "The Madu Wedding",
-    link: "https://tochiandyvonne.onrender.com/",
+    title: "FitTrack Site",
+    link: "https://www.fittracker.us",
     image: {
-      src: `${maduwedding}`,
-      alt: "Photo of wedding site"
+      src: `${fittrack}`,
+      alt: "Photo of fit track site"
     },
-    skills: ["HTML", "CSS", "Javascript", "Express.js", "Bootstrap", "Render"],
-    description: `Multipage static website for 
-          wedding that lays out details and links guests to external sites.`,
+    skills: [
+      "React", "MySQL", "React Router", "NGINX",
+      "HTML", "CSS", "Javascript", "Express.js",
+       "Bootstrap", "Render", "AWS RDS",
+    ],
+    description: `Fully functional responsive web app for planning and recording daily workouts.`,
   },
   {
     id: 2,
+    title: "My Portfolio",
+    link: "https://www.darryladigwu.com",
+    image: {
+      src: `${portfolio_site}`,
+      alt: "Photo of wedding site"
+    },
+    skills: ["React", "Docker", "Express.js", "HTML", "CSS", "Javascript", "Bootstrap", "Render"],
+    description: `Portfolio site.
+    `,
+  },
+  {
+    id: 3,
+    title: "The Madu Wedding",
+    link: "https://tochiandyvonne.onrender.com/",
+    image: {
+      src: `${maduwedding}`,
+      alt: "Photo of wedding site"
+    },
+    skills: ["HTML", "CSS", "Javascript", "Express.js", "EJS", "Bootstrap", "Render"],
+    description: `Multipage static website for 
+          wedding that lays out details and links guests to external sites.`,
+  },
+  {
+    id: 4,
     title: "The Madu Wedding",
     link: "https://tochiandyvonne.onrender.com/",
     image: {
@@ -26,7 +54,19 @@ export default [
           wedding that lays out details and links guests to external sites.`,
   },
   {
-    id: 3,
+    id: 5,
+    title: "The Madu Wedding",
+    link: "https://tochiandyvonne.onrender.com/",
+    image: {
+      src: `${maduwedding}`,
+      alt: "Photo of wedding site"
+    },
+    skills: ["HTML", "CSS", "Javascript", "Express.js", "Bootstrap", "Render"],
+    description: `Multipage static website for 
+          wedding that lays out details and links guests to external sites.`,
+  },
+  {
+    id: 6,
     title: "The Madu Wedding",
     link: "https://tochiandyvonne.onrender.com/",
     image: {
